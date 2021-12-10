67256c5cb5855e1f41527ea4697034d82121aeda
kwanhwijoo <81539175+kwanhwijoo@users.noreply.github.com>
2021-12-11 04:40:35 +0900

    Update ssh_test_first.py

--- ssh_test_first.py
+++ ssh_test_first.py
@@ -9,13 +9,6 @@ start_time = time.time()
 cli = paramiko.SSHClient()
 cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
  
-# server = input("211.253.29.136")  # 호스트명이나 IP 주소
-# user = input("root")  
-# pwd = getpass.getpass("qwer43@!") # 암호입력 숨김
-# server = input("Server: ")  # 호스트명이나 IP 주소
-# user = input("Username: ")  
-# pwd = getpass.getpass("Password: ") # 암호입력 숨김
-# cli.connect(server, port=7777, username=user, password=pwd)
 
 cli.connect("xxx", port=xxx, username="xxxx", password="xxxx")
 
